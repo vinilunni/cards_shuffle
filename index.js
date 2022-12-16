41333476b96593108f50401def40e876d678e6f1
VINIL VARGHESE <vinilunni@gmail.com>
2022-12-16 17:13:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const cards_shuffle = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello cards_shuffle`}))
+  res.end()
+  
+}
+
+export default cards_shuffle
